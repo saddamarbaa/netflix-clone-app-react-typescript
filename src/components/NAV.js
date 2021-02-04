aaa77281a278b715b98f4baf869d4d0d39340d17
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2021-02-04 18:17:16 +0700

    ADD:Show/hide scrollNavBar

--- src/components/NAV.js
+++ src/components/NAV.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./NAV.css";
 import logo from "../images/logo.png";
 import profile from "../images/profile__logo.png";
@@ -6,8 +6,33 @@ import profile from "../images/profile__logo.png";
 // App NavBar(child  component)
 
 const NAV = () => {
+  // blow  for hide and show functionality
+
+  const [show, handleShowNav] = useState(false);
+
+  const transitionNavBar = () => {
+    if (window.scrollY > 100) {
+      handleShowNav(true);
+    } else {
+      handleShowNav(false);
+    }
+  };
+
+  // add scroll EventListener and call transitionNavBar() function
+  // (Show/hide scrollbar)
+  // the code will run only when the component found
+
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNavBar);
+    return () => {
+      // cleanup after done clean up
+      window.removeEventListener("scroll", transitionNavBar);
+    };
+  }, []);
+
   return (
-    <div className="nav nav__black">
+    // (add or render nav__black to the webside) only if the show variable is true
+    <div className={`nav ${show && "nav__black"}`}>
       <div className="nav__contents">
         <img className="nav__logo" src={logo} alt="logo" />
         <img className="nav__profile" src={profile} alt="profile" />
