56f4ec862651d9b1b28788d82492ddc5df0cab2a
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2021-02-05 00:41:45 +0700

    ADD:Row component

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./HomeScreen.css";
 import Banner from "./Banner";
 import NAV from "./NAV";
+import request from "./Requests";
+import Row from "./Row";
 
 // HomeScreen component
 
@@ -10,6 +12,20 @@ const HomeScreen = () => {
     <div className="homeScreen">
       <NAV />
       <Banner />
+      <Row
+        title="NETFILX ORIGINALS"
+        fetchUrl={request.fetchNetflixOriginals}
+        isLageRow
+      />
+      <Row title="Top Rated" fetchUrl={request.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={request.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={request.fetchNetflixOriginals} />
+      <Row title="Horror Movies" fetchUrl={request.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={request.fetchRomanceMovies} />
+      <Row
+        title="Documentaries Movies"
+        fetchUrl={request.fetchDocumentariesMovies}
+      />
     </div>
   );
 };
---
+++ src/components/Row.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Row.css";
+
+// Row component
+
+function Row() {
+  return <div></div>;
+}
+
+export default Row;
