372159ead86237a953cec137e29ddef541f6ae30
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2021-02-04 23:18:38 +0700

    ADD:dynamic image for the Banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Banner.css";
+import axios from "./axios";
+import requests from "./Requests";
 
 // Banner component
 
 const Banner = () => {
+  // fetch movies from TMDB saved in movie variable and used for Banner(dynamic)
+  //Initialize movie variable with empty array
+
+  const [movie, setMovie] = useState([]);
+
+  // (useEffect) to fetch movies information
+
+  useEffect(() => {
+    async function fetchMoviesData() {
+      // waits until the request completes..
+      // (requests.fetchNetflixOriginals) is coming from Requests.js file
+
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      // console.log(request);
+      // console.log(request.data);
+      // console.log(request.data.results);
+      // console.log(request.data.results.length);
+
+      // generate random number from zero to the numbers of the movies that come from API
+      //  and save the movie which at that number to movie variable
+
+      const randomMovieNumber = Math.floor(
+        Math.random() * request.data.results.length - 1
+      );
+      // console.log("Random number is ", randomMovieNumber);
+      setMovie(request.data.results[randomMovieNumber]);
+      return request;
+    }
+
+    // fetchMoviesData()
+    fetchMoviesData();
+  }, []);
+
+  // console.log(movie);
+
   /**
    * function to truncate(cut) the string if the length of given string bigger    than  given number(n)
    *  @param {string}  given string
@@ -24,9 +61,10 @@ const Banner = () => {
   return (
     <header
       className="banner"
+      // {inline styling}
       style={{
         backgroundSize: "cover",
-        backgroundImage: `url("https://assets.brand.microsites.netflix.io/assets/493f5bba-81a4-11e9-bf79-066b49664af6_cm_1440w.png")`,
+        backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`,
         backgroundPosition: "center center",
       }}
     >
