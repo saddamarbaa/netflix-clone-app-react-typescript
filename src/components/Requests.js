946bab9cdf804a3a737852f4d6a624f9fb3d6cc4
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2021-02-04 20:50:49 +0700

    ADD:request file to fetch movies from(TMDb)

---
+++ src/components/Requests.js
@@ -0,0 +1,21 @@
+// TMDB API KEY
+// Typically we would stroe in {process.env.API_KEY}
+const API_KEY = "92bcc12799d8068995c7c9650f414f3e";
+
+// function to fetch movies from (TMDb)
+
+const request = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres = 28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres = 35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres = 27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres = 10749`,
+  fetchDocumentariesMovies: `/discover/movie?api_key=${API_KEY}&with_genres = 99`,
+};
+
+// this is what is happening for each request
+// https://api.themoviedb.org/3/trending/all/week?api_key=92bcc12799d8068995c7c9650f414f3e&language=en-US
+
+export default request;
