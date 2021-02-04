02141197e04f4e3388ab3dd7ee5c9ba7298c7d1b
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2021-02-04 13:18:45 +0700

    ADD:HomeScreen component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 
+// App component(parent component)
+
 function App() {
   return (
     <div className="app">
-      <h1>Lets build Netflix</h1>
+      <HomeScreen />
     </div>
   );
 }
---
+++ src/HomeScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./HomeScreen.css";
+
+// App HomeScreen(parent component)
+
+const HomeScreen = () => {
+  return <div className="homeScreen"></div>;
+};
+
+export default HomeScreen;
