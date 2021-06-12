8419f89add96d84895ff7385a8e8fd6fd34bddad
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2021-06-12 21:15:40 +0700

    add firebase config

---
+++ src/config/firebase.js
@@ -0,0 +1,10 @@
+/** @format */
+
+const firebaseConfig = {
+	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+	projectId: process.env.REACT_APP_PROJECT_ID,
+	storageBucket: "netflix-clone-80070.appspot.com",
+	messagingSenderId: "22307329868",
+	appId: "1:22307329868:web:3c974d3021728ba0a6a1e9",
+};
