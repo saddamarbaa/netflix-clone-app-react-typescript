be28be00b7d38b6c245e97c647169aa4c9aa9b8a
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2021-06-14 13:10:12 +0700

    feat: add youtube trailer pop-ups functionality

--- src/components/Row.js
+++ src/components/Row.js
@@ -5,12 +5,15 @@ import "./Row.css";
 import axios from "../api/axios";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 const Row = ({ title, fetchUrl, isLargeRow = false }) => {
 	// let create peace of state to keep trake of the movies
 	// fetch movies from TMDB saved in movies variable and used for Rows(dynamic)
 	// Initialize movies variable with empty array
 	const [movies, setMovies] = useState([]);
+	const [trailerUrl, setTrailerUrl] = useState("");
 	const base_url = "https://image.tmdb.org/t/p/original";
 
 	// function  to fetch movies information
@@ -29,12 +32,42 @@ const Row = ({ title, fetchUrl, isLargeRow = false }) => {
 		fetchMoviesData();
 	}, [fetchUrl]);
 
+	//This  From react-youtube documentation
+	const opts = {
+		height: "390",
+		width: "100%",
+
+		playerVars: {
+			// https://developers.google.com/youtube/player_parameters
+			autoplay: 1,
+		},
+	};
+
+	// Handle Youtube Trailer
+	// This from movie-trailer documentation
+	const handleClick = (movie) => {
+		if (trailerUrl) {
+			setTrailerUrl("");
+		} else {
+			movieTrailer(movie.name || "")
+				.then((fullUrl) => {
+					if (fullUrl) {
+						const urlParams = new URL(fullUrl).search;
+						const urlSearchParams = new URLSearchParams(urlParams);
+						const movieIdOnYoutube = urlSearchParams.get("v");
+						setTrailerUrl(movieIdOnYoutube);
+					}
+				})
+				.catch((error) => {
+					console.log(error);
+				});
+		}
+	};
+
 	return (
 		<div className='row'>
 			<h2>{title}</h2>
-
 			{/* {iterate over the movie object and return row of image} */}
-
 			<div className='row__posters'>
 				{movies?.map(
 					(movie) =>
@@ -43,6 +76,7 @@ const Row = ({ title, fetchUrl, isLargeRow = false }) => {
 							<LazyLoadImage
 								effect='blur'
 								placeholderSrc='https://image.tmdb.org/t/p/original/4EYPN5mVIhKLfxGruy7Dy41dTVn.jpg'
+								onClick={() => handleClick(movie)}
 								className={`row__poster ${
 									isLargeRow && "row__posterLarge"
 								}`}
@@ -55,6 +89,8 @@ const Row = ({ title, fetchUrl, isLargeRow = false }) => {
 						),
 				)}
 			</div>
+
+			{trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
 		</div>
 	);
 };
