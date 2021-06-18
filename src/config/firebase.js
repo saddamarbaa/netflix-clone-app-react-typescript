fc89d6dc8da4176bf11ec66da49ff9527d56e28e
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2021-06-18 20:48:03 +0700

    feat: add firebase config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,10 +1,21 @@
 /** @format */
+import firebase from "firebase";
+import "firebase/firestore";
 
 const firebaseConfig = {
 	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
 	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
 	projectId: process.env.REACT_APP_PROJECT_ID,
-	storageBucket: "netflix-clone-80070.appspot.com",
-	messagingSenderId: "22307329868",
-	appId: "1:22307329868:web:3c974d3021728ba0a6a1e9",
+	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_MESSAIN_SENDER_ID,
+	appId: process.env.REACT_APP_APPID,
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+
+export default db;
