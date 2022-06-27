229d596a96acd730c9b19a7148f86828e894ec11
saddamarbaa <saddam@rushowl.sg>
2022-06-27 23:14:16 +0700

    chore: hide firebase keys

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -15,24 +15,12 @@ import { getFirestore } from 'firebase/firestore'
 const googleProvider = new GoogleAuthProvider()
 
 const firebaseConfig = {
-	apiKey: 'AIzaSyDmVpIn92XJcSdaZQLPwSpfDpYVx83DKLA',
-
-	authDomain: 'signal-clone-app-reactnative.firebaseapp.com',
-
-	projectId: 'signal-clone-app-reactnative',
-
-	storageBucket: 'signal-clone-app-reactnative.appspot.com',
-
-	messagingSenderId: '1068718271502',
-
-	appId: '1:1068718271502:web:e70d419019f50c5ee0a05f',
-
-	// apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-	// authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-	// projectId: process.env.REACT_APP_PROJECT_ID,
-	// storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-	// messagingSenderId: process.env.REACT_APP_MESSAIN_SENDER_ID,
-	// appId: process.env.REACT_APP_APPID,
+	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+	projectId: process.env.REACT_APP_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_MESSAIN_SENDER_ID,
+	appId: process.env.REACT_APP_APPID,
 }
 
 // Initialize Firebase
