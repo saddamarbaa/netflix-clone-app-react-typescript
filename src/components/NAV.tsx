6c36ea7d2b78efaf3da76944ff511976a28fe77b
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2024-12-22 11:31:32 -0800

    feat: integrate youtube video trailer api

--- src/components/NAV.tsx
+++ src/components/NAV.tsx
@@ -6,7 +6,6 @@ import profile from '../assets/images/profile__logo.png'
 
 const NAV = () => {
 	const history = useNavigate()
-
 	const [show, handleShowNav] = useState(false)
 
 	const transitionNavBar = () => {
@@ -17,9 +16,6 @@ const NAV = () => {
 		}
 	}
 
-	// add scroll EventListener and call transitionNavBar() function
-	// (Show/hide scrollbar)
-	// the code will run only when the component found
 	useEffect(() => {
 		window.addEventListener('scroll', transitionNavBar)
 
@@ -28,21 +24,20 @@ const NAV = () => {
 	}, [])
 
 	return (
-		// (add or render nav__black to the website) only if the show variable is true
 		<div
 			className={`nav fixed top-0 p-5 w-full h-[30px] z-10 transition-all duration-500 ${
-				show ? 'bg-gray-900' : ''
+				show ? 'bg-gray-900 h-[40px]' : ''
 			}`}>
-			<div className="nav__contents flex justify-between">
+			<div className="nav__contents flex justify-between cursor-pointer">
 				<img
 					onClick={() => history('/')}
-					className="nav__logo fixed top-3 left-0 w-[80px] cursor-pointer pl-5"
+					className="fixed top-3 left-0 w-[95px] pl-5 cursor-pointer"
 					src={logo}
 					alt="logo"
 				/>
 				<img
 					onClick={() => history('/profile')}
-					className="nav__profile fixed top-3 right-5 w-[30px] cursor-pointer"
+					className="fixed top-3 right-5 w-[30px] cursor-pointer"
 					src={profile}
 					alt="profile"
 				/>
